4d7258997be7cfe05396d7aeadfa004f0c9fdf57
Eugene <Eugene@DESKTOP-CE9GMNF>
2023-02-16 23:40:31 +0800

    Добавьте файлы проекта.

---
+++ MineSweeper/Field.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    internal class Field
+    {
+        public int m { get; set; }
+        public int n { get; set; }
+        public int p { get; set; }
+        public char[,] matrix { get; set; }
+
+        public Field(int m, int n, int p)
+        {
+            this.m = m;
+            this.n = n;
+            matrix = new char[n, m];
+
+            this.p = p;
+        }
+        private void fill()
+        {
+            Random rnd =  new Random();
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    int num = rnd.Next(100);
+                    if(num <= p)
+                    {
+                        matrix[i, j] = '*';
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] != '*')
+                    {
+                        if (i == 0)
+                        {
+                            if (j == 0)
+                            {
+
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ MineSweeper/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MineSweeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные(m,n,p) через пробел: ");
+            
+            string input = Console.ReadLine();
+            string[] values = input.Split(' ');
+
+            int.TryParse(values[0], out int m);
+            int.TryParse(values[1], out int n);
+            int.TryParse(values[2], out int p);
+
+            Field field = new Field(m, n, p);
+
+        }
+    }
+}
